ff5dfeceb12badaa980ea7bf3d0ebe0a0433b71d
Ethan P <eth-p+git@hidden.email>
2023-03-25 01:07:05 -0700

    feat: Make fetchObsidianStyles more robust
    A new fallback method is used to fetch the CSS rules straight from the
    document.styleSheets object. We lose the formatting and comments, but
    it's better than nothing.

--- src/functions/fetchObsidianStyles.ts
+++ src/functions/fetchObsidianStyles.ts
@@ -1,4 +1,4 @@
-import { App as ObsidianApp, apiVersion } from 'obsidian';
+import { App as ObsidianApp, Platform, apiVersion } from 'obsidian';
 import { Latest, Version } from 'obsidian-undocumented';
 
 import { Electron, Global } from '../internal/types/electron';
@@ -12,37 +12,87 @@ import { versionCompare } from '../internal/utils/versionCompare';
  * @returns The CSS stylesheet contents.
  */
 export default async function fetchObsidianStyles<V extends Version = Latest>(app: ObsidianApp): Promise<string> {
-	try {
-		return await (await fetch("/app.css")).text();
-	} catch (_ex) {}
+	const orElse = (cb: () => string | Promise<string>) => (_ex: unknown) => {
+		// console.debug(
+		// 	'[obsidian-extra]: One method used to fetch Obsidian styles failed. This is OK, there are more.\n',
+		// 	ex,
+		// );
+		return cb();
+	};
 
-	// Fallback: Use Electron APIs.
+	return fetchObsidianStylesViaElectron('app.css')
+		.catch(orElse(() => fetchObsidianStylesViaFetch('app.css')))
+		.catch(orElse(() => fetchObsidianStylesViaReconstruction('app.css')));
+}
+
+/**
+ * Try to fetch the styles with a fetch request to `/app.css`.
+ * This does not work for the Electron-based desktop app.
+ */
+async function fetchObsidianStylesViaFetch(path: string): Promise<string> {
+	if (Platform.isDesktopApp) {
+		// If we know it doesn't work, we might as well preemptively stop it
+		// so we don't spam the console with fetch error messages.
+		throw new Error('Obsidian styles via fetch() does not work under Electron.');
+	}
+
+	return fetch(`/${path}`).then((r) => r.text());
+}
+
+/**
+ * Try to fetch the styles using the Electron APIs.
+ *
+ * This must be checked for every version, as `sendSync` may hang the web process if the IPC call
+ * doesn't return anything. It also doesn't work for the mobile apps.
+ */
+async function fetchObsidianStylesViaElectron(path: string): Promise<string> {
 	if (versionCompare(apiVersion, '1.1.15') > 0) {
-		throw new Error(`[obsidian-extra]: Obsidian ${apiVersion} has not been tested with this function`);
+		throw new Error(`Obsidian ${apiVersion} has not been tested with this function`);
 	}
 
-	try {
-		const require = (globalThis as Global).require;
+	const require = (globalThis as Global).require;
 
-		// We need "electron" to get the path to the Obsidian asar, and we need "fs" to read the asar.
-		// Note: "fs" was patched by Electron to be able to read asars.
-		const electron: Electron | undefined = (globalThis as Global).electron ?? require?.('electron');
-		if (electron == null) {
-			throw new Error('Unable to get electron module from web renderer process');
-		}
-
-		const fs = require?.('fs/promises');
-		if (fs?.readFile == null) {
-			throw new Error('Unable to get fs/promises module from web renderer process');
-		}
-
-		// Get the path to the Obsidian asar.
-		// SAFETY: If there is no listener on the main process side, THIS WILL HANG THE WEB VIEW.
-		const resources = electron.ipcRenderer.sendSync('resources');
-
-		// Get the contents of the `app.css` resource.
-		return await fs.readFile(`${resources}/app.css`, 'utf8');
-	} catch (ex) {
-		throw new Error(`[obsidian-extra]: Could not get Obsidian styles: ${ex}`);
+	// We need "electron" to get the path to the Obsidian asar, and we need "fs" to read the asar.
+	// Note: "fs" was patched by Electron to be able to read asars.
+	const electron: Electron | undefined = (globalThis as Global).electron ?? require?.('electron');
+	if (electron == null) {
+		throw new Error('Unable to get electron module from web renderer process');
 	}
+
+	const fs = require?.('fs/promises');
+	if (fs?.readFile == null) {
+		throw new Error('Unable to get fs/promises module from web renderer process');
+	}
+
+	// Get the path to the Obsidian asar.
+	// SAFETY: If there is no listener on the main process side, THIS WILL HANG THE WEB VIEW.
+	const resources = electron.ipcRenderer.sendSync('resources');
+
+	// Get the contents of the `app.css` resource.
+	return fs.readFile(`${resources}/${path}`, 'utf8');
+}
+
+/**
+ * Try to reconstruct the stylesheet using DOM APIs.
+ * This works, but we lose formatting and comments.
+ */
+function fetchObsidianStylesViaReconstruction(path: string): string {
+	let found = false;
+	const lines = [];
+
+	for (const styleSheet of Array.from(document.styleSheets)) {
+		// Try to find the <link> element for the obsidian `app.css` stylesheet.
+		if (!(styleSheet.ownerNode instanceof HTMLLinkElement)) continue;
+		const href = styleSheet.ownerNode.getAttribute('href');
+		if (href !== path && href !== `/${path}`) continue;
+
+		// Add all the CSS rules to an array.
+		found = true;
+		for (const rule of Array.from(styleSheet.cssRules)) {
+			lines.push(rule.cssText);
+		}
+	}
+
+	if (!found) throw new Error("Unable to find <link> element for Obsidian's stylesheet");
+	return lines.join('\n');
 }
